adbd1be86dc75d61b9879488436fcdc0eb326f77
maheshkumar <maheshkumar@bicsglobal.com>
2024-11-19 12:37:58 +0530

    ec2 node type changed

--- 7-nodes.tf
+++ 7-nodes.tf
@@ -45,7 +45,7 @@ resource "aws_eks_node_group" "general" {
   ]
 
   capacity_type  = "ON_DEMAND"
-  instance_types = ["t3.small"]
+  instance_types = ["t3.medium"]
 
   scaling_config {
     desired_size = 2
